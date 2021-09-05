4da0b8ca894160ee5895399fa0cc86d9b66551ef
Markus Stäuble <staeuble@gmail.com>
2021-09-05 21:06:08 +0200

    Aktualisiere main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,11 @@
+let x = 0
+let y = 0
 basic.forever(function () {
-	
+    led.plot(x, y)
+    basic.pause(200)
+    led.unplot(x, y)
+    if (x == 4) {
+        y = (y + 1) % 5
+    }
+    x = (x + 1) % 5
 })
